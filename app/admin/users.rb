827447612a9d2d3ed8d634a2d4512743290fbf56
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-09 19:40:44 -0600

    Adding resource to active admin Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com> Co-authored-by: LPMendoza <lpedromendoza333@gmail.com> Co-authored-by: EdsonMorfin <edsonpaul2018@gmail.com>

---
+++ app/admin/users.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  #permit_params :email, 
+  #:password, 
+  #:institute_id, 
+  #:section_id, 
+  #:reports, 
+  #:reset_password_token, 
+  #:reset_password_sent_at, 
+  #:remember_created_at
+  #
+  # or
+  #
+  permit_params do
+    permitted = [:email, :password, :institute_id, :section_id, :reports, :reset_password_token, :reset_password_sent_at, :remember_created_at]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  form partial: "form"
+
+  
+  
+end
