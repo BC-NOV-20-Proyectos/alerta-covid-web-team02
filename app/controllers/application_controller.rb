d47e83332bbb66892e16e4196a7a31aab0ca3fad
MiguelOcegueraM <migueloceguera.m@gmail.com>
2021-03-10 18:58:28 -0600

    We did the mailer
    Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>
    Co-authored-by: EdsonMorfin <edsonpaul2018@gmail.com>
    Co-authored-by: jnegrete15 <jnegrete15@ucol.mx>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+    after_action :send_email, only: [:create]
     protected
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:institute_id, :section_id, :reports])
+    end
+
+    def send_email
+      UserMailer.send_key(params[:user]).deliver
     end
 end
