a5c69afd4e9c9056178f7ffb06e8e9c366a75fc0
MiguelOcegueraM <migueloceguera.m@gmail.com>
2021-03-11 19:03:28 -0600

    We generated the mails with the password
    Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>
    CO-authored-by: EdsonMorfin <edsonpaul2018@gmail.com>
    Co-authored-by: EdsonMorfin <edsonpaul2018@gmail.com>
    Co-authored-by: jnegrete15 <jnegrete15@ucol.mx>

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   resources :sections
   resources :places
   resources :institutes
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   ActiveAdmin.routes(self)
-
-  post '/users', to: 'users/registrations#create'
+  
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
