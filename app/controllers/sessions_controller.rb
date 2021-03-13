bbb43df65cd9f8029324602ec6768ba2017b1bb2
Luis Pedro Mendoza Aceves <lpedromendoza333@gmail.com>
2021-03-12 18:58:27 -0600

    Login by jwt
    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com>
    Co-authored-by: jnegrete15 <jnegrete15@ucol.mx>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 class SessionsController < Devise::SessionsController  
     skip_before_action :verify_authenticity_token
-    def new
-        render json: {"hola": "hola"}
-    end
+    
     def create
         user = User.find_by_email(sign_in_params[:email])
 
