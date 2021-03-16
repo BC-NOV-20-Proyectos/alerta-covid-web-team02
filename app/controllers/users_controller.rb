84f4842209fbfde5f1d97de37e7414e18accc1e2
Luis Pedro Mendoza Aceves <lpedromendoza333@gmail.com>
2021-03-16 17:48:56 -0600

    Login with JWT
    Co-authored-by: jnegrete15 <jnegrete15@ucol.mx>
    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com>
    Co-authored-by: EdsonMorfin <edsonpaul2018@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ActionController::Base
-    before_action :authenticate_user!, only :get_user
+    before_action :authenticate_user!, only: :get_user
 
     def get_user
         render json: {"name": "user"}
