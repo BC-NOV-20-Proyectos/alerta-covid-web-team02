892fed519f760352b7b1a8b28c5b7ab3fe4b0c2e
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-25 16:23:59 -0600

    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com> Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 class SessionsController < Devise::SessionsController  
-    #skip_before_action :verify_authenticity_token, raise: false
+    skip_before_action :verify_authenticity_token, raise: false
     
-    skip_before_action :verify_authenticity_token
-    skip_before_action :authenticate_user!, :only => "reply", :raise => false
+    # skip_before_action :verify_authenticity_token
+    # skip_before_action :authenticate_user!, :only => "reply", :raise => false
 
     respond_to :json
     private
