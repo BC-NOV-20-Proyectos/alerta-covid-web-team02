c262bde6ca68cc2e5be3cc3a20718bef2c60df42
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-25 16:29:09 -0600

    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com> Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :process_token
   # def access_denied(exception)
   #   redirect_to root_path, alert: exception.message
