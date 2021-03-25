772b009f7e49678d0acc5300129147c425a7fa0f
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-24 19:23:36 -0600

    stil fixing issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :process_token
-  def access_denied(exception)
-    redirect_to root_path, alert: exception.message
-  end
+  # def access_denied(exception)
+  #   redirect_to root_path, alert: exception.message
+  # end
 
   private
 
