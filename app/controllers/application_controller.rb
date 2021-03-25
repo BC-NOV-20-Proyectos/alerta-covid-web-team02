973ae3c10fe3f72930c4d529c78736d8c7b860c2
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-25 16:05:28 -0600

    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com> Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery unless: -> { request.format.json? }
   before_action :process_token
   # def access_denied(exception)
   #   redirect_to root_path, alert: exception.message
   # end
 
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
   private
 
