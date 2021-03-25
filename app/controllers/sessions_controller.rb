941e662bb01eb58b7a0887374892788ddaec7fdb
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-25 16:08:42 -0600

    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com> Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class SessionsController < Devise::SessionsController  
-    skip_before_action :verify_authenticity_token
+    #skip_before_action :verify_authenticity_token
     respond_to :json
     private
     def respond_with(resource, _opts = {})
