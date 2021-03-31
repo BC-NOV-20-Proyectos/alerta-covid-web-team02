a6a7d87cd1b00f8047387da166b4d99dffa91104
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-03-31 17:40:56 -0600

    Place reports done
    Co-authored-by: MiguelOcegueraM <migueloceguera.m@gmail.com>
    Co-authored-by: LPMendoza <lpedromendoza333@gmail.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,9 +4,15 @@ class ReportsController < ApplicationController
     def user_report
         time_to = (Time.now + 1.day).strftime("%Y-%m-%d")
         time_from = 10.days.ago.strftime("%Y-%m-%d")
+        
+        if params[:time_from] and params[:time_to]
+           time_to = params[:time_to]
+           time_from = params[:time_from]
+        end
+        
         @incidents = Incident.where("created_at BETWEEN '#{time_from.to_s}'::timestamp AND '#{time_to.to_s}'::timestamp")
         @users_report = format_user_report
-        render json:{data: @users_report}
+        
     end
 
     def format_user_report
@@ -28,6 +34,29 @@ class ReportsController < ApplicationController
     def places_report
         time_to = (Time.now + 1.day).strftime("%Y-%m-%d")
         time_from = 10.days.ago.strftime("%Y-%m-%d")
-        @incidents = Incident.where("created_at BETWEEN '#{time_from.to_s}'::timestamp AND '#{time_to.to_s}'::timestamp")
+        
+        if params[:time_from] and params[:time_to]
+            time_to = params[:time_to]
+            time_from = params[:time_from]
+         end
+        
+        @incidents = Incident.select("place_id, SUM(symptomatic) AS symptomatic, SUM(covid_positive) AS positive , SUM(covid_negative) AS negative").where("created_at between '#{time_from}'::timestamp and '#{time_to}'::timestamp").order('positive DESC, symptomatic DESC, negative DESC').group(:place_id);
+        render json:{data: format_place_report}
+    end
+
+    def format_place_report
+        report_array = []
+        @incidents.each do |incident| 
+            json_incident = {
+                "institute": incident.place.institute.name,
+                "area": incident.place.area,
+                "name": incident.place.name,
+                "symptomatic": incident[:symptomatic],
+                "covid_positive": incident[:positive] == nil ? 0 : incident[:positive],
+                "covid_negative": incident[:negative] == nil ? 0 : incident[:negative]               
+            }
+            report_array << json_incident
+        end
+        report_array
     end
 end
